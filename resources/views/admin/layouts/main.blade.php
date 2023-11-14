abbca194481f1046001a1ffad1b224c4079e79e5
Bünyamin Göymen <55482246+bunyamingoymen@users.noreply.github.com>
2023-11-14 11:58:57 +0300

    Admin Template Yüklendi
    Apaxy adındaki admin template parçalandı ve projeye entegre edildi. Footer Kısmı Güncellendi

---
+++ resources/views/admin/layouts/main.blade.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Anikagai | Admin </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Anikagai admin created by bunyamingoymen" name="description" />
+    <meta content="Bünyamin Göymen" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../../../admin/assets/images/favicon.ico">
+
+    <!-- Bootstrap Css -->
+    <link href="../../../admin/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="../../../admin/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="../../../admin/assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body data-sidebar="dark">
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+        @include("admin.layouts.topbar")
+
+        <!-- ========== Left Sidebar Start ========== -->
+        @include("admin.layouts.sidebar")
+        <!-- Left Sidebar End -->
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-flex align-items-center justify-content-between">
+                                <h4 class="mb-0 font-size-18">Starter Page</h4>
+
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Apaxy</a></li>
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                        <li class="breadcrumb-item active">Starter Page</li>
+                                    </ol>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                    <div>
+                        @yield("admin_content")
+                    </div>
+
+                </div> <!-- container-fluid -->
+            </div>
+            <!-- End Page-content -->
+
+
+            @include('admin.layouts.footer');
+        </div>
+        <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+
+    <!-- JAVASCRIPT -->
+    <script src="../../../admin/assets/libs/jquery/jquery.min.js"></script>
+    <script src="../../../admin/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../../admin/assets/libs/metismenu/metisMenu.min.js"></script>
+    <script src="../../../admin/assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="../../../admin/assets/libs/node-waves/waves.min.js"></script>
+
+    <script src="../../../admin/assets/js/app.js"></script>
+
+</body>
+
+</html>
