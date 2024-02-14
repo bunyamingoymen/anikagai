c14ec03ad2247e5a884be64e29c7fc55b3b8212f
Bünyamin Göymen <55482246+bunyamingoymen@users.noreply.github.com>
2024-02-14 10:07:14 +0300

    Sitemap güncellendi
    Sitemap güncellendi

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,13 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        'App\Console\Commands\GenerateSitemap',
+    ];
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('sitemap:generate')->daily();
+        $schedule->command('sitemap:generate')->everyFiveMinutes();
     }
 
     /**
