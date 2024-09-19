0c46b717695a4cb8e97d93fb98201f636aaf3dbc
Bünyamin Göymen <bunyamingoymen@tutanota.com>
2024-09-19 12:14:21 +0300

    İstek listesi tablosu eklendi

---
+++ app/Models/Shop/ShopWhishlist.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Shop;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShopWhishlist extends Model
+{
+    use HasFactory;
+}
